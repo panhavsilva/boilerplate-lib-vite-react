9628c7b773dd7ac2df47d6046efd91719c273a9c
pamela vieira da silva <panhavsilva@gmail.com>
2022-03-12 15:37:05 -0300

    :heavy_plus_sign: adiciona vite-plugin-dts e @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { EsLinter, linterPlugin } from 'vite-plugin-linter'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig(configEnv => ({
@@ -12,5 +14,22 @@ export default defineConfig(configEnv => ({
       include: ['./{lib,src}/**/*.{ts,tsx}'],
       linters: [new EsLinter({ configEnv })],
     }),
+    dts({
+      include: ['lib/main.tsx'],
+      beforeWriteFile: (filePath, content) => ({
+        filePath: filePath.replace('/lib', ''),
+        content,
+      }),
+    }),
   ],
+  build: {
+    lib: {
+      entry: resolve('lib', 'main.tsx'),
+      name: 'ReactFeatureFlag',
+      fileName: (format) => `my-lib-hello.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react'],
+    },
+  },
 }))
